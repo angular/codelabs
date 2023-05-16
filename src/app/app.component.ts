f86c03cf6896c1b2c4c28a5147a7ccb639cd3390
Mark Thompson <2554588+MarkTechson@users.noreply.github.com>
2023-05-15 17:01:12 -0700

    initial commit of the homes app codelab

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-root',
+  template: `<h1>Hello World!</h1>`,
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'homes';
+}
