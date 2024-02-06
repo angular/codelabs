6c8b75787b7a6060245a13fe27cc483f705a2a42
Jemel Watson <113080066+watdevs@users.noreply.github.com>
2024-02-05 22:15:28 -0600

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   standalone: true,
   selector: 'app-root',
-  template: `<h1>Hello World!!</h1>`,
+  template: `
+  <main>
+    <header class="brand-name">
+      <img src="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+    </header>
+    </main>
+    `,
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
